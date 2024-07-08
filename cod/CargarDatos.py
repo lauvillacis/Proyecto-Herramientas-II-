d6069293f48ff52ed35141ef0a2b927b028c03d8
lauvillacis <lauvillacisd@gmail.com>
2024-07-08 08:56:42 -0600

    feat - get y set de CargarDatos

--- cod/CargarDatos.py
+++ cod/CargarDatos.py
@@ -12,5 +12,39 @@ class CargarDatos():
     def __init__(self, base):
         self.__base = base
         
+    @property
+    def base(self):
+        '''
+        Es el metodo get, para devolver el valor del atributo 'base'
+        
+        Parametros
+        -------
+        No lleva parametros explicitos pero se usa el objeto de la clase
+        
+        Returns
+        -------
+        No devuelve nada
+        '''
+        return self.__base
+    
+    
+    #Set
+    @base.setter
+    def base(self, nueva_base):
+        '''
+        Es el metodo set, para establecer el valor del atributo 'base'
+        
+        Parametros
+        -------
+        nueva_base : dataframe
+            Esto es el  dataframe que se quiere asignar/ para modificar 
+            el atributo 'base' del objeto de la clase
+        
+        Returns
+        -------
+        No devuelve nada
+        '''
+        self.__base = nueva_base
+        
     def cargar_base(self, ruta):
         self.__base = pd.read_excel(ruta)
